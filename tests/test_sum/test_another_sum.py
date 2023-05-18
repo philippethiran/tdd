92403729cc9c946a0c2a5f07036faabc05de7415
philippethiran <pthiran@gmail.com>
2023-05-18 10:05:04 +0200

    Adding unittest 2 Addition + 10

--- tests/test_sum/test_another_sum.py
+++ tests/test_sum/test_another_sum.py
@@ -1,9 +1,12 @@
 import unittest
-from sum.another_sum import another_sum
+from sum.another_sum import another_sum, adD10
 
 class MyTestCase(unittest.TestCase):
     def test_another_sum(self):
         assert another_sum(3, 2) == 5
+
+    def test_one10(self):
+        assert adD10(3) == 13
 
 if __name__ == '__main__':
     unittest.main()
