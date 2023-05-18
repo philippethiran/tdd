8dc1b32e710e8757e7f128b973cceda343da4902
philippethiran <pthiran@gmail.com>
2023-05-18 18:49:23 +0200

    Correction after review

--- sum/another_sum.py
+++ sum/another_sum.py
@@ -6,16 +6,17 @@ def add_10(a):
     return a + 10
 
 
-def add_serie(a, b):
+def add_series(a, b):
     y = 0
     for x in range(a, b + 1):
         y = y + x
     return y
 
-def add_even_serie(a, b):
+
+def add_even_series(a, b):
     # this function add only the even numbers between a and b
     y = 0
-    if (a % 2) != 0 :  # a is not even
+    if (a % 2) != 0:  # a is not even
         a = a + 1      # we start then with the first pair after a
     for x in range(a, b+1, 2):
         y = y + x
--- tests/test_sum/test_another_sum.py
+++ tests/test_sum/test_another_sum.py
@@ -1,5 +1,6 @@
 import unittest
-from sum.another_sum import another_sum, add_10, add_serie, add_even_serie
+from sum.another_sum import another_sum, add_10, add_series, add_even_series
+
 
 class MyTestCase(unittest.TestCase):
     def test_another_sum(self):
@@ -11,14 +12,15 @@ class MyTestCase(unittest.TestCase):
     def test_add_neg(self):
         assert add_10(-3) == 7
 
-    def test_add_serie(self):
-        assert add_serie(3, 5) == 12
+    def test_add_series(self):
+        assert add_series(3, 5) == 12
 
-    def test_add_event_serie(self):
-        assert add_even_serie(3, 5) == 4
-        assert add_even_serie(2, 5) == 6
-        assert add_even_serie(2, 6) == 12
-        assert add_even_serie(-1, 6) == 12
+    def test_add_event_series(self):
+        assert add_even_series(3, 5) == 4
+        assert add_even_series(2, 5) == 6
+        assert add_even_series(2, 6) == 12
+        assert add_even_series(-1, 6) == 12
+
 
 if __name__ == '__main__':
     unittest.main()
