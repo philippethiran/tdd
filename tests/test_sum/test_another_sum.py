b1c5a92760839a7c1a787d400990cb795aea2e90
philippeunamur <133955749+philippeunamur@users.noreply.github.com>
2023-05-18 10:34:03 +0200

    Update test_another_sum.py
    Change the function name + add a test with a negative number

--- tests/test_sum/test_another_sum.py
+++ tests/test_sum/test_another_sum.py
@@ -1,12 +1,15 @@
 import unittest
-from sum.another_sum import another_sum, adD10
+from sum.another_sum import another_sum, add_10
 
 class MyTestCase(unittest.TestCase):
     def test_another_sum(self):
         assert another_sum(3, 2) == 5
 
-    def test_one10(self):
-        assert adD10(3) == 13
+    def test_add_3(self):
+        assert add_10(3) == 13
+        
+   def test_add_neg(self):
+        assert add_10(-3) == 7
 
 if __name__ == '__main__':
     unittest.main()
