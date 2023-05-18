13698714f0d38f5fbddbec8f86907067b72309a6
philippethiran <pthiran@gmail.com>
2023-05-18 18:32:33 +0200

    Adding unittest 2 Addition + 10

--- sum/another_sum.py
+++ sum/another_sum.py
@@ -1,5 +1,22 @@
 def another_sum(a, b):
     return a + b
 
+
 def add_10(a):
     return a + 10
+
+
+def add_serie(a, b):
+    y = 0
+    for x in range(a, b + 1):
+        y = y + x
+    return y
+
+def add_even_serie(a, b):
+    # this function add only the even numbers between a and b
+    y = 0
+    if (a % 2) != 0 :  # a is not even
+        a = a + 1      # we start then with the first pair after a
+    for x in range(a, b+1, 2):
+        y = y + x
+    return y
--- tests/test_sum/test_another_sum.py
+++ tests/test_sum/test_another_sum.py
@@ -1,15 +1,24 @@
 import unittest
-from sum.another_sum import another_sum, add_10
+from sum.another_sum import another_sum, add_10, add_serie, add_even_serie
 
 class MyTestCase(unittest.TestCase):
     def test_another_sum(self):
         assert another_sum(3, 2) == 5
 
-    def test_add_3(self):
+    def test_add_10(self):
         assert add_10(3) == 13
         
     def test_add_neg(self):
         assert add_10(-3) == 7
+
+    def test_add_serie(self):
+        assert add_serie(3, 5) == 12
+
+    def test_add_event_serie(self):
+        assert add_even_serie(3, 5) == 4
+        assert add_even_serie(2, 5) == 6
+        assert add_even_serie(2, 6) == 12
+        assert add_even_serie(-1, 6) == 12
 
 if __name__ == '__main__':
     unittest.main()
